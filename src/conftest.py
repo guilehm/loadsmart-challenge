e7f91ae6b800e668fe5de8defbb6a188fb488a3b
guilherme <guile.hm@hotmail.com>
2020-08-05 16:38:43 -0300

    create conftest and requirements

---
+++ src/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+
+from src.models import Truck, Cargo, Combination
+
+
+@pytest.fixture
+def truck_1():
+    return Truck(_id=1, lat='35', lng='-87')
+
+
+@pytest.fixture
+def truck_2():
+    return Truck(_id=2, lat='40', lng='-87')
+
+
+@pytest.fixture
+def cargo_1():
+    return Cargo(_id=1, origin_lat='41', origin_lng='-87')
+
+
+@pytest.fixture
+def cargo_2():
+    return Cargo(_id=2, origin_lat='41.5', origin_lng='-87.5')
+
+
+@pytest.fixture
+def combination_1(truck_1, cargo_1):
+    return Combination(truck=truck_1, cargo=cargo_1)
+
+
+@pytest.fixture
+def combination_2(truck_2, cargo_2):
+    return Combination(truck=truck_2, cargo=cargo_2)
